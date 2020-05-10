8321bcadaa38d3c642bb2601343408c06b82b925
Aashwath <aashwath.acharya@gmail.com>
2020-05-10 22:33:41 +0530

    use local storage

--- src/Cart.js
+++ src/Cart.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CartItem from './CartItem';
 import './Cart.css'
 
 function Cart({initialItems}){
-
-	const [items, setItems] = useState(initialItems);
+	const initialState = JSON.parse(window.localStorage.getItem('items'))
+	const [items, setItems] = useState(initialState || initialItems);
 	
+	useEffect(()=>{
+		window.localStorage.setItem('items', JSON.stringify(items));
+	}, [items])
+
 	const updateQty = (id, newQty) => {
 		const newItems = items.map(item => {
 			if(item.id === id) return {...item, qty: newQty}
